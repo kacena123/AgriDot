2b12cf2e8af1ec2bbafb4fed0273db72ec2a3214
kacena123 <73550330+kacena123@users.noreply.github.com>
2024-09-29 14:30:22 +0200

    adding tailwind

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,11 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 const profile = () => {
   return (
-    <View style={styles.container}>
-      <Text>Profile</Text>
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView>
+        <ScrollView contentContainerStyle={{height: '100%'}}>
+          <View style={styles.container}>
+            <Text>Profile</Text>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </GestureHandlerRootView>
   )
 }
 
@@ -13,9 +20,9 @@ export default profile
 
 const styles = StyleSheet.create({
   container:{
-    display: 'flex',
-    flex: 1,
+    width: '100%',
+    justifyContent: 'center',
     alignItems: 'center',
-    justifyContent: 'center'
-  }
+    padding: 80,
+  },
 })
