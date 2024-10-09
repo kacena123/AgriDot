0310175d32aad6fc789ee179e7bd142e8dafb6f4
kacena123 <73550330+kacena123@users.noreply.github.com>
2024-10-09 22:58:53 +0200

    myGuides screen

---
+++ app/(guides)/addGuide.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const addGuide = () => {
+  return (
+    <View>
+      <Text>addGuide</Text>
+    </View>
+  )
+}
+
+export default addGuide
+
+const styles = StyleSheet.create({})
---
+++ app/(guides)/detailGuide.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const detailGuide = () => {
+  return (
+    <View>
+      <Text>detailGuide</Text>
+    </View>
+  )
+}
+
+export default detailGuide
+
+const styles = StyleSheet.create({})
