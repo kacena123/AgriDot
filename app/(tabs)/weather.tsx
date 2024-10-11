02c66ab25252eb391e0cccb36bae65355cd0e523
kacena123 <73550330+kacena123@users.noreply.github.com>
2024-10-11 12:11:31 +0200

    picker for location in weather screen

--- app/(tabs)/weather.tsx
+++ app/(tabs)/weather.tsx
@@ -1,7 +1,8 @@
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { Picker } from '@react-native-picker/picker';
 
 // Importing screens
 import CurrentWeather from '@/app/(weather)/currentWeather';
@@ -10,8 +11,30 @@ import LongtermWeather from '@/app/(weather)/longtermWeather';
 
 const weather = () => {
   const TopTab = createMaterialTopTabNavigator();
+
+  const [selectedLocation, setSelectedLocation] = useState<string | null>(null);
+  const locations = ['Location 1', 'Location 2', 'Location 3']; // Replace with your actual locations
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
+
+        {/* Picker for selecting location */}
+        <View style={{ paddingHorizontal: 10 }}>
+        <View style={styles.pickerContainer}>
+            <Picker
+                selectedValue={selectedLocation}
+                onValueChange={(itemValue) => {
+                    setSelectedLocation(itemValue)
+                }}
+                style={{ fontFamily: 'DMSans', fontSize: 15}}
+                dropdownIconColor="#FD47B7"
+            >
+                {locations.map((location, index) => (
+                    <Picker.Item key={index} label={location} value={location} />
+                ))}
+            </Picker>
+        </View>
+        </View>
 
       {/* Top tab navigation */}
       <TopTab.Navigator
@@ -45,5 +68,14 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
-  }
+  }, 
+  pickerContainer: {
+    borderColor: 'rgba(253, 71, 183, 0.23)',
+    backgroundColor: 'rgba(253, 71, 183, 0.23)',
+    borderWidth: 1,
+    borderRadius: 20,
+    paddingHorizontal: 0,
+    marginVertical: 10,
+    minHeight: 40,
+  },
 })
