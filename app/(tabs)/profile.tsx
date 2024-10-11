783c8e9b94fa8f14b80daf5eb2299de61343cdf5
kacena123 <73550330+kacena123@users.noreply.github.com>
2024-10-11 11:39:41 +0200

    edit profile screen

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,7 +1,5 @@
-import { ScrollView, StyleSheet, Text, View, Image, ImageBackground  } from 'react-native'
+import { ScrollView, StyleSheet, Text, View, Image, ImageBackground, SafeAreaView } from 'react-native'
 import React from 'react'
-import { GestureHandlerRootView } from 'react-native-gesture-handler'
-import { SafeAreaView } from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar'
 import { Link } from 'expo-router'
 import CustomButton from '@/components/CustomButton'
@@ -11,46 +9,84 @@ import CustomButton from '@/components/CustomButton'
 const profile = () => {
 
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <SafeAreaView className='bg-white h-full'>
-        
-        <ScrollView contentContainerStyle={{height: '100%'}}>
-        <ImageBackground source={require('@/assets/images/profil_screen2.png')} >
-          
-          <View className="w-full h-full items-center py-20">
-            <View className='items-center'>
-              <Text style={styles.text}>Don't know what Polkadot is?</Text>
-              <Text style={styles.textLink}> <Link href='https://polkadot.network/'>Click here to learn more.</Link></Text>
-            </View>
-            <View className="w-full" style={{ flex: 1, justifyContent: 'flex-end' }}>
-              <View className='items-center w-full px-6'>
+    <ScrollView style={styles.wrapper}>
+    <SafeAreaView>
+      <View style={{ width: '100%' }}>
+        <Image
+          source={require('@/assets/images/green_rectangle.png')}
+          style={styles.topImage}
+          resizeMode="cover"
+        />
+        <Image
+          source={require('@/assets/images/secured_by_polkadot.png')}
+          style={styles.topImageOverlay}
+          resizeMode="contain"
+        />
+      </View>
+
+      <View style={styles.imageRow}>
+        <ImageBackground
+          source={require('@/assets/images/Polygon1.png')}
+          style={styles.sideImage}
+          resizeMode="contain"
+        />
+        <ImageBackground
+          source={require('@/assets/images/Polygon2.png')}
+          style={styles.sideImage}
+          resizeMode="contain"
+        />
+      </View>
+
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>Don't know what Polkadot is?</Text>
+        <Text style={styles.textLink}> <Link href='https://polkadot.network/'>Click here to learn more.</Link></Text>
+      </View>
+
+      <View style={{ alignItems: 'center', marginTop: 200 }}>
+        <Image
+          source={require('@/assets/images/logo.png')}
+          style={{ width: 300, height: 70 }}
+          resizeMode="contain"
+        />
+      </View>
+
+      <View style={styles.textContainer2}>
                 <Text style={styles.text2}>Want to support our app?</Text>
                 <CustomButton 
                   title='Donate' 
                   onPress={() => {console.log('tap')} }
-                  containerStyles={{ height: 55 }}
+                  containerStyles={{ height: 55, marginTop: 10 }}
                   textStyles={{ fontSize: 18, fontFamily: 'DMSans' }}
                 />
                 <CustomButton 
                   title='Log out' 
                   onPress={() => {console.log('tap')}} 
-                  containerStyles={{ backgroundColor: '#145E2F', height: 55 }}
+                  containerStyles={{ backgroundColor: '#145E2F', height: 55, marginTop: 30 }}
                   textStyles={{ fontSize: 18, fontFamily: 'DMSans' }}
                 />
               </View>
-            </View>
-          </View>
-          <StatusBar style="light" backgroundColor='#145E2F'/>
-          </ImageBackground>
-        </ScrollView>
-      </SafeAreaView>
-    </GestureHandlerRootView>
+      <StatusBar style="light" backgroundColor='#145E2F'/>
+    </SafeAreaView>
+    </ScrollView>
   )
 }
 
 export default profile
 
 const styles = StyleSheet.create({
+  wrapper: {
+    backgroundColor: '#fff',
+    flex: 1,
+  },
+  textContainer: {
+    marginTop: 60,
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  textContainer2: {
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
   text: {
     fontSize: 18,
     fontFamily: 'DMSans',
@@ -66,5 +102,25 @@ const styles = StyleSheet.create({
     color: '#FD47B7',
     textDecorationLine: 'underline',
   },
-  
+  topImage: {
+    width: '100%',
+    height: 100,
+    position: 'absolute',
+    top: 0,
+  },
+  topImageOverlay: {
+    alignSelf: 'center',
+    top: 40,
+  },
+  imageRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    position: 'absolute',
+    marginTop: 50,
+  },
+  sideImage: {
+    width: 150,
+    height: 450,
+  },
 })
